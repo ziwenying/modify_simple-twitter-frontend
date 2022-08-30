767b7b3ccbafa870ab188243fc7b8cad7b5a08c1
ziwenying <s316108@gmail.com>
2022-08-30 14:35:29 +0800

    fix: URL

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/simple-twitter-frontend/'
+    ? '/modify_simple-twitter-frontend/'
     : '/',
   css: { extract: false },
 }
